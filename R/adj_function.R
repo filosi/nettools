81d55d9b8b46059c9241179b774894dda1e3c08b
filosi <filosi@fbk.eu>
2013-08-20 10:31:43 +0200

    Added function "checkvar" in file adj_function.R for checking feature with low variance in matrixor dataframes.

--- R/adj_function.R
+++ R/adj_function.R
@@ -161,3 +161,17 @@ AdjDTWMIC <- function(x,DP,...){
     return(Adj)
 }
 
+## Function for check the variance by features
+checkvar <- function(x, tol=1e-5){
+  
+  ## Compute the variance by columns
+  feat.var <- apply(x,2,var)
+  
+  ## Get the indexes of the feature with low variance
+  idx <- which(feat.var<tol)
+  if (length(idx) == 0L){
+    return (NULL)
+  } else {
+    return(idx)
+  }
+}
