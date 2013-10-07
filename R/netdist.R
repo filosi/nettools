e27c635cad1414f653a11461204728a4eced20cc
filosi <filosi@fbk.eu>
2013-10-07 14:50:41 +0200

    If names will be provided they will propagate in the distance matrix

--- R/netdist.R
+++ R/netdist.R
@@ -208,10 +208,25 @@ netdist.list <- function(x, d="HIM", ga=NULL, components=TRUE, ...){
     mylap <- list(L=laplist,N=N, tag=tag)
     ## mylap <- list(L1=Lap(g1$adj),L2=Lap(g2$adj),N=g1$N, tag=g1$tag)
     dd <- ipsen(mylap,ga=ga, ...)
+    ## if (!is.null(names(x)))
+    ##   colnames(dd) <- rownames(dd) <- names(x)
   }
   if (myadj$d=="H"){
     dd <- hamming(myadj)
+    ## if (!is.null(names(x)))
+    ##   colnames(dd) <- rownames(dd) <- names(x)
   }
+  
+  ## Give names to the matrices
+  if (!is.null(names(x))){
+    if (is.list(dd)){
+      dd <- lapply(dd,function(y,x){colnames(y) <- rownames(y) <- names(x)
+                                    return(y)}, x=x)
+    } else {
+      colnames(dd) <- rownames(dd) <- names(x)
+    }
+  }
+  
   return(dd)
 }
 setMethod("netdist", "list", netdist.list)
@@ -296,8 +311,9 @@ him <- function(object,ga=NULL, components=TRUE, ltag=FALSE, ...){
       names(dist) <- c("H","IM","HIM")
     }
   } else {
-    dist <- list(HIM=gloc)
-    ## names(dist) <- "HIM"
+    dist <- gloc
+    if (!ltag)
+      names(dist) <- "HIM"
   }
   return(dist)
 }
