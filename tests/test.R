8db5adaf9f90935d9366df7715ce1e1231c17b23
Michele Filosi <filosi@fbk.eu>
2014-08-28 10:16:06 +0200

    Remove igraph test in test file

--- tests/test.R
+++ tests/test.R
@@ -10,15 +10,15 @@ b <- matrix(rnorm(1000),ncol=100)
 
 ## Compute adjacency matrix!
 ## For different methods check the mat2adj help page!
-aadj <- mat2adj(a, method="MINE")
-badj <- mat2adj(b, method="MINE")
+aadj <- mat2adj(a, method="MINE", n.cores=1)
+badj <- mat2adj(b, method="MINE", n.cores=1)
 
 ## Compute the him distance between the 2 matrix
 
 ## Him distance
-netdist(aadj,badj,method="HIM",n.cores=1,components=FALSE)
+netdist(aadj,badj, method="HIM", components=FALSE, n.cores=1)
 ## HIM distance using list of matrices as input
-netdist(list(aadj,badj),method="HIM", components=FALSE, n.cores=1)
+netdist(list(aadj,badj), method="HIM", components=FALSE, n.cores=1)
 
 
 ## Hamming distance
@@ -34,16 +34,16 @@ netdist(aadj,badj,method="ips",gamma=0.09, n.cores=1)
 ##-------------------------------------------------
 ## Using igraph
 ##-------------------------------------------------
-require(igraph)
+## require(igraph)
 
-g1 <- barabasi.game(200,0.8)
-g2 <- barabasi.game(200,0.2)
+## g1 <- barabasi.game(200,0.8)
+## g2 <- barabasi.game(200,0.2)
 
-a1 <- get.adjacency(g1,type="both",sparse=TRUE)
-a2 <- get.adjacency(g2,type="both",sparse=TRUE)
+## a1 <- get.adjacency(g1,type="both",sparse=TRUE)
+## a2 <- get.adjacency(g2,type="both",sparse=TRUE)
 
 ## HIM distance
-netdist(g1,g2,method="HIM", n.cores=1)
+## netdist(g1,g2,method="HIM", n.cores=1)
 
 
 ##-------------------------------------------------
