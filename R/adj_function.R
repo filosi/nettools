72e218d17f8732a24a96407b401f21ca3eb9b9bc
filosi <filosi@fbk.eu>
2014-10-08 18:30:34 +0200

    Added use arguments to all adj functions

--- R/adj_function.R
+++ R/adj_function.R
@@ -43,14 +43,14 @@ AdjWGCNA <- function(x,P,...){
                            corFnc = 'cor',
                            corOptions=paste("use=\'", use,"\'", sep=""))
   diag(Adj) <- 0
-
+  
   ## use argument, remove NA from Adj and set to 0
   na.method <- pmatch(use, c("all.obs", "complete.obs", "pairwise.complete.obs", 
                              "everything"))
   if (na.method %in% c(2L, 3L))
     Adj[is.na(Adj)] <- 0
 
-  ## return adjacency matrix
+  ## return Adj
   return(Adj)
 }
 
@@ -222,68 +222,109 @@ AdjCLR <- function(x,...){
 
 ## MINE
 AdjMINE <- function(x,measure,alpha,C, var.thr=1e-10,...){
-  Adj <- mine(x,alpha=alpha, C=C,var.thr=var.thr)[[measure]]
-  if (!is.null(Adj)){
-    diag(Adj) <- 0
-    return(Adj)
+  ## Check for use argument in ...
+  param <- list(...)
+  nn <- names(param)
+  if (!is.na(match("use", nn))){
+    use <- param$use
   } else {
+    use <- "all.obs"
+  }
+
+  ## Infer the adjacency matrix
+  Adj <- mine(x,alpha=alpha, C=C,var.thr=var.thr, ...)[[measure]]
+  
+  ## Check whether the measure for MINE is available 
+  if (is.null(Adj)){
     stop("Invalid measure for method MINE")
+  } else {
+    ## Set diagonal to 0, no auto-loop
+    diag(Adj) <- 0
+
+    ## use argument, remove NA from Adj and set to 0
+    na.method <- pmatch(use, c("all.obs", "complete.obs", "pairwise.complete.obs", 
+                               "everything"))
+    if (na.method %in% c(2L, 3L))
+      Adj[is.na(Adj)] <- 0
+    return(Adj)
   }
 }
 
 ## MINEFDR
 AdjMINEFDR <- function(x,measure,alpha,C,FDR,...){
-    Adj <- AdjMINE(x,measure,alpha,C,...)
-    idx <- as.matrix(expand.grid(seq(dim(Adj)[1]),seq(dim(Adj)[2])))
-
-    ## check for multicore
-    if (!is.na(match("n.cores", names(list(...))))){
-      n.cores <- list(...)[["n.cores"]]
-      if (is.numeric(n.cores) && n.cores > 1 && detectCores()>1){
-        if (n.cores > detectCores())
-          n.cores <- detectCores() - 1
-        cl <- makeCluster(n.cores)
-      } else {
-        cl <- NULL
-      }
+  ## Compute the step 0 for FDR filter
+  Adj <- AdjMINE(x,measure,alpha,C,...)
+  idx <- as.matrix(expand.grid(seq(dim(Adj)[1]),seq(dim(Adj)[2])))
+  
+  ## check for multicore
+  if (!is.na(match("n.cores", names(list(...))))){
+    n.cores <- list(...)[["n.cores"]]
+    if (is.numeric(n.cores) && n.cores > 1 && detectCores()>1){
+      if (n.cores > detectCores())
+        n.cores <- detectCores() - 1
+      cl <- makeCluster(n.cores)
     } else {
       cl <- NULL
     }
-    
-    for (i in seq(1/FDR)){
-      if (nrow(idx) > 2){
-        cormat <- fdrrun(x,idx,FUN='mine',measure=measure, cl,...)
-        idx <- which(Adj>cormat,arr.ind=TRUE)
-      }
+  } else {
+    cl <- NULL
+  }
+  
+  for (i in seq(1/FDR)){
+    if (nrow(idx) > 2){
+      cormat <- fdrrun(x,idx,FUN='mine',measure=measure, cl,...)
+      idx <- which(Adj>cormat,arr.ind=TRUE)
     }
-    adjfinal <- matrix(0,ncol=dim(Adj)[2],nrow=dim(Adj)[1],
-                       dimnames=list(rownames(Adj),colnames(Adj)))
-    if(nrow(idx)>2){
-        for (i in seq(dim(idx)[1])){
-            adjfinal[idx[i,1],idx[i,2]] <- Adj[idx[i,1],idx[i,2]]
-        }
+  }
+  adjfinal <- matrix(0,ncol=dim(Adj)[2],nrow=dim(Adj)[1],
+                     dimnames=list(rownames(Adj),colnames(Adj)))
+  if(nrow(idx)>2){
+    for (i in seq(dim(idx)[1])){
+      adjfinal[idx[i,1],idx[i,2]] <- Adj[idx[i,1],idx[i,2]]
     }
-    return(adjfinal)
+  }
+  return(adjfinal)
 }
 
 
 ## DTWMIC
 AdjDTWMIC <- function(x,DP,...){
-    Adj <- matrix(0,nrow=ncol(x),ncol=ncol(x))
-    for(i in 1:(ncol(x)-1)){
-        for(j in (i+1):ncol(x)){
-            d <- 1/(1+dtw(x[,i],x[,j],distance.only=TRUE)$normalizedDistance)
-            m <- mine(x[,i],x[,j])$MIC
-            Adj[i,j] <- Adj[j,i]<- sqrt(d**2/2+m**2/2)**DP
-        }
+  ## Check for use argument in ...
+  param <- list(...)
+  nn <- names(param)
+  if (!is.na(match("use", nn))){
+    use <- param$use
+  } else {
+    use <- "all.obs"
+  }
+
+  
+  ## Infer the adjacency matrix
+  Adj <- matrix(0,nrow=ncol(x),ncol=ncol(x))
+  for(i in 1:(ncol(x)-1)){
+    for(j in (i+1):ncol(x)){
+      d <- 1/(1+dtw(x[,i],x[,j],distance.only=TRUE)$normalizedDistance)
+      m <- mine(x[,i],x[,j])$MIC
+      Adj[i,j] <- Adj[j,i]<- sqrt(d**2/2+m**2/2)**DP
     }
-    rownames(Adj) <- colnames(Adj) <- colnames(x)
-    return(Adj)
+  }
+  rownames(Adj) <- colnames(Adj) <- colnames(x)
+
+  ## use argument, remove NA from Adj and set to 0
+  na.method <- pmatch(use, c("all.obs", "complete.obs", "pairwise.complete.obs", 
+                             "everything"))
+  if (na.method %in% c(2L, 3L))
+    Adj[is.na(Adj)] <- 0
+
+  ## Return Adj
+  return(Adj)
 }
 
 ## Function for computing the FDR value on the null hypothesis
-fdrrun <- function(x,idx,FUN='cor', cl=NULL, use="pairwise.complete.obs",...){
+fdrrun <- function(x,idx,FUN='cor', cl=NULL, ...){
   FUN <- match.fun(FUN)
+
+  ## Set the measure for MINE statistics
   measure <- 1
   if (!is.na(match("measure",names(list(...))))){
     measure <- list(...)[["measure"]]
@@ -291,20 +332,31 @@ fdrrun <- function(x,idx,FUN='cor', cl=NULL, use="pairwise.complete.obs",...){
       measure <- 1
   }
 
+  ## Check for use argument in ...
+  param <- list(...)
+  nn <- names(param)
+  if (!is.na(match("use", nn))){
+    use <- param$use
+  } else {
+    use <- "all.obs"
+  }
+
+  ## Allocate space for the matrix
   cormat <- matrix(Inf,ncol=dim(x)[2],nrow=dim(x)[2])
   myfun <- function(x, y, idx, tmpfun, measure, use){
     idi <- idx[x,]
     if (idi[1]>idi[2]){
-      ## cat(idi, "\n")
-      return(abs(tmpfun(sample(y[,idi[2]]),y[,idi[1]], use)[[measure]]))
-    }else{return(NA)}
+      return(abs(tmpfun(sample(y[,idi[2]]),y[,idi[1]], use=use)[[measure]]))
+    } else {
+      return(NA)
+    }
   }
   if(nrow(idx)>2){
     if (is.null(cl)){
-      aa <- sapply(seq(dim(idx)[1]),myfun, y=x, idx=idx, tmpfun=FUN, measure, use)
+      aa <- sapply(seq(dim(idx)[1]),myfun, y=x, idx=idx, tmpfun=FUN, measure=measure, use=use)
     } else {
       ## NB need to load package on all the cores?!??!?!
-      aa <- parSapply(cl, seq(dim(idx)[1]),myfun, y=x, idx=idx, tmpfun=FUN, measure, use)
+      aa <- parSapply(cl, seq(dim(idx)[1]),myfun, y=x, idx=idx, tmpfun=FUN, measure=measure, use=use)
     }
     idx <- cbind(idx,aa)
     tmpid <- subset(idx,!is.na(idx[,3]))
@@ -326,6 +378,6 @@ checkvar <- function(x, tol=1e-5, ...){
   if (length(idx) == 0L){
     return (NULL)
   } else {
-    return(idx)
+    return (idx)
   }
 }
