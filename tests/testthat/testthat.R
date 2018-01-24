0d725e3512907475bba2cd1595ba04529e5c9a5a
filosi <michele.filosi@unitn.it>
2018-01-24 10:27:09 +0100

    update tests

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -43,21 +43,43 @@ test_that("Test empty-full distance:", {
     expect_equal(as.numeric(dd), 1)
 })
 
-
 test_that("Test distance:", {
     aadj <- mat2adj(a, infer.method="WGCNA", n.cores=1)
     badj <- mat2adj(b, infer.method="WGCNA", n.cores=1)
     d1 <- netdist(aadj,badj, d="HIM", components=FALSE, n.cores=1)
     d2 <- netdist(list(aadj,badj), d="HIM", components=FALSE, n.cores=1)
+    expect_equal(d1,d2)
+})
+
+test_that("Test Directed Distance:", {
+    aadj <- mat2adj(a, infer.method="WGCNA", n.cores=1)
+    badj <- mat2adj(b, infer.method="WGCNA", n.cores=1)
+    n <- nrow(aadj)
+    
+    myga <- nettools:::optimal_gamma(n)
+    
+    bipaadj <- matrix(0, nrow=2*n, ncol=2*n)
+    bipaadj[1:n, (n+1):(2*n)] <- t(aadj)
+    bipaadj[(n+1):(2*n), 1:n] <- aadj
+
+    bipbadj <- matrix(0, nrow=2*n, ncol=2*n)
+    bipbadj[1:n, (n+1):(2*n)] <- t(badj)
+    bipbadj[(n+1):(2*n), 1:n] <- badj
+    
+    d1 <-  netdist(aadj,badj, d="HIM", components=FALSE, n.cores=1)
+    d2 <- netdist(bipaadj, bipbadj, d="HIM", components=FALSE, n.cores=1)
+    expect_equal(d1, d2)
 })
 
 test_that("Test SI:", {
+    skip("Do not test multicore...")
     ssind1 <- netSI(a, adj.method="ARACNE", n.cores=4, use="pair", symm=TRUE)
     ssind2 <- netSI(a,  adj.method="ARACNE", n.cores=4, use="pair", symm=TRUE)
     expect_equal(length(ssind1$Sw), length(ssind2$Sw))
 })
 
 test_that("Test SI 2:", {
+    skip("Do not test multicore...")
     ssind1 <- netSI(a, adj.method="ARACNE", n.cores=1, use="pair", symm=TRUE)
     ssind2 <- netSI(a, n.cores=1, adj.method="ARACNE", use="pair", symm=TRUE)
     expect_equal(length(ssind1$Sw), length(ssind2$Sw))
